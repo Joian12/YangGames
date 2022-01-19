ed3eea0eb1f6b1ee33c7e08ae034401f6be91560
Joian12 <49311808+Joian12@users.noreply.github.com>
2022-01-19 00:54:34 -0800

    ....
    ....

--- YangGame/index.js
+++ YangGame/index.js
@@ -1,7 +1,9 @@
 
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(75, 1, 0.1, 2000);
+cam.position.set(0,0.5,10);
 var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor('0xFFFFFF');
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const loader = new THREE.GLTFLoader();
@@ -10,3 +12,14 @@ loader.load('Robot.gltf', function ( gltf ) {
     var robot = gltf.scene;
 	scene.add(robot);
 } );
+
+var globalLight = new THREE.AmbientLight(0x808080, 1)
+scene.add(globalLight);
+
+function animate()
+{
+    requestAnimationFrame(animate);
+    renderer.render(scene, cam);
+}
+
+animate();
